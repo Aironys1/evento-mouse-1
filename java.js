430e10e9ec90b57d5b9c1025c3e774e0c6fb6ef6
Aironys Garrido <aironys1@hotmail.com>
2022-03-15 13:51:10 -0300

    Incluido alguns projetos

--- java.js
+++ java.js
@@ -6,3 +6,11 @@ function clicou2(){
     document.getElementById("h3").innerHTML ="Parabéns pelo seu interesse. Deixe um Ok para saber mais. "
 }
 
+function digitou(){
+    document.getElementById("teclado").innerHTML = "Vem clicando";
+    console.log("Segurando a tecla. ");
+}
+function digitou2(){
+    document.getElementById("teclado2").innerHTML = "Clicando tecla por tecla";
+    console.log("Clicando tecla por tecla");
+}
